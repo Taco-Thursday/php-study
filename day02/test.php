dd0d7e2d2a9e4e92a8958892c4def4b253526b33
gaopengfei <gaopengfei@shinetechsoftware.com>
2021-01-13 14:07:18 +0800

    improve day02 test

--- day02/test.php
+++ day02/test.php
@@ -1,73 +1,67 @@
 <?php
-	
-	/*有三个学生，每个学生有两门课，英语和数学，每门课都有成绩，比如 jack math: 90, engilsh:76。
+
+/*有三个学生，每个学生有两门课，英语和数学，每门课都有成绩，比如 jack math: 90, engilsh:76。
 	要求：
 	1. 用php的数组描述出来刚才的话
 	2. 写一个方法求平均分最高的学生名字  getMaxScoreStudent()
 	3. 一个是求数学的最低分，一个把英语最低分的人名打印出来*/
-	$students = [
-		"jack" => [
-			"Math" => 100,
-			"English" => 55,
-		],
-		"lucy" => [
-			"Math" => 105,
-			"English" => 35,
-		],
-		"anny" => [
-			"Math" => 66,
-			"English" => 85,
-		],
-	];
-	# var_dump($students);
-	# 平均分最高的学生名字
-	function getMaxScoreStudent($students)
-	{
-		$result = '';
-		$maxMeanScore = ($students['jack']['Math'] + $students['jack']['English']) / 2;
-		foreach ($students as $name => $value) {
-			$meanScore = ($value['Math'] + $value['English']) / 2;
+$students = [
+	"jack" => [
+		"Math" => 100,
+		"English" => 55,
+	],
+	"lucy" => [
+		"Math" => 95,
+		"English" => 95,
+	],
+	"anny" => [
+		"Math" => 106,
+		"English" => 85,
+	],
+];
+# var_dump($students);
+# 平均分最高的学生名字
+function getMaxScoreStudent($students)
+{
+	$result = '';
+	$maxMeanScore = 0;
+	foreach ($students as $name => $value) {
+		$meanScore = ($value['Math'] + $value['English']) / 2;
 
-			if($meanScore > $maxMeanScore) {
-				$maxMeanScore = $meanScore;
-			}
-
-			if($maxMeanScore == $meanScore){
-				$result = $name;
-			}
+		if ($meanScore > $maxMeanScore) {
+			$maxMeanScore = $meanScore;
+			$result = $name;
 		}
-		echo "$maxMeanScore";
-		return $result;
 	}
-	# getMaxScoreStudent($students);
-	echo getMaxScoreStudent($students);
+	return $result;
+}
+# getMaxScoreStudent($students);
+echo getMaxScoreStudent($students);
 
-	# 数学的最低分
-	function getMinScore($students)
-	{
-		$mathScore = $students['jack']['Math'];
-		foreach ($students as $name => $value) {
-			if($value['Math'] < $mathScore){
-				$mathScore = $value['Math'];
-			}
+# 数学的最低分
+function getMinScore($students)
+{
+	$mathScore = $students['jack']['Math'];
+	foreach ($students as $name => $value) {
+		if ($value['Math'] < $mathScore) {
+			$mathScore = $value['Math'];
 		}
-		return $mathScore;
 	}
-	echo getMinScore($students);
+	return $mathScore;
+}
+echo getMinScore($students);
 
-	# 英语最低分的名字
-	function getMinEnglishStudent($students)
-	{
-		$minEnglishName = '';
-		$englishScore = $students['jack']['Math'];
-		foreach ($students as $name => $value) {
-			if($value['English'] < $englishScore){
-				$englishScore = $value['English'];
-			}
-			if($englishScore == $value['English']){
-				$minEnglishName = $name;
-			}
+# 英语最低分的名字
+function getMinEnglishStudent($students)
+{
+	$minEnglishName = '';
+	$englishScore = $students['jack']['English'];
+	foreach ($students as $name => $value) {
+		if ($value['English'] < $englishScore) {
+			$englishScore = $value['English'];
+			$minEnglishName = $name;
 		}
-		return $minEnglishName;
 	}
-	echo getMinEnglishStudent($students);
+	return $minEnglishName;
+}
+echo getMinEnglishStudent($students);
